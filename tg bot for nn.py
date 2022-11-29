b584639b4800786d38a286f5b7f26ed8ee9af9fd
orlov artem <85007266+Hedge-creator@users.noreply.github.com>
2022-11-29 21:58:55 +0300

    add keyboardbutton

--- tg bot for nn.py
+++ tg bot for nn.py
@@ -1,7 +1,7 @@
 import telebot
-import cv2
 import time
 import logging
+from telebot import types
 
 token = '5478353034:AAGQ5cFEBHgHVh8YT345aKRYwcNO0AgbJ3g' #подключаем токен бота с помощью токена, полученного от BotFather
 bot = telebot.TeleBot(token)
@@ -10,14 +10,21 @@ bot = telebot.TeleBot(token)
 
 def start_message(message): #при нажатии команды /start, бот вежливо здоровается, назвав нас по имени
     #ёщё он предлагает список своих услуг
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    item1 = types.KeyboardButton("Сравнение лиц на схожесть")
+    item2 = types.KeyboardButton("Поиск лиц на фотографии")
+    item3 = types.KeyboardButton("Возраст человека, пол, раса")
+
+    markup.add(item1, item2, item3)
+
     user_id = message.from_user.id
     user_full_name = message.from_user.full_name
     logging.info(f'{user_id=} {user_full_name=}', time.asctime())
-    bot.send_message(message.chat.id,f"приветствую, {user_full_name}! ✌")
-    bot.send_message(message.chat.id, "отправь мне фотографию, на которой я постараюсь отыскать лица! 😛")
+    
+    bot.send_message(message.chat.id,f"приветствую, {user_full_name}! ✌", reply_markup=markup)
 @bot.message_handler(content_types=['photo'])
 def get_img_message(message): #зададим функцию, цели которой будет реакция бота на полученное изображение
     image_path = message.photo
     bot.send_message(message.from_user.id, "Принято! 🤗")
   
-bot.infinity_polling()
+bot.polling(none_stop=True)
