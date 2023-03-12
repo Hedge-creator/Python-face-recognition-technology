e7afd16a708a78d6ca744bf12786d82496dfa77b
Hedge-creator <85007266+Hedge-creator@users.noreply.github.com>
2023-03-12 19:06:07 +0300

    Update nn on mtcnn.py

--- nn on mtcnn.py
+++ nn on mtcnn.py
@@ -1,25 +1,25 @@
-#импорт необходимых библиотек
-from mtcnn.mtcnn import MTCNN #библиотека ии
-from matplotlib import pyplot # бибилиотека, нужная для вывода изображения на экран компьютера
-from matplotlib.patches import Rectangle #также подключаем библиотеку, способную рисовать квадраты вокруг найдённых лиц
-#задаём функцию для создания квадратов
+#импорт библиотек
+from mtcnn.mtcnn import MTCNN 
+from matplotlib import pyplot 
+from matplotlib.patches import Rectangle 
+#функция для создания квадратов
 def draw_image_with_boxes(image_path, result_list):
-	data = pyplot.imread(image_path) #чтение полученного изображения
+	data = pyplot.imread(image_path) #чтение изображения
 	pyplot.imshow(data) #построение картинки в pyplot
 	ax = pyplot.gca()
 	#нанесение квадратов на график
 	for result in result_list:
-		#узнаем координаты лиц
+		#координаты лиц
 		x, y, width, height = result['box']
-		#создаём форму
+		#форма фигуры
 		rect = Rectangle((x, y), width, height, fill=False, color='aqua') #можно задать толщину линии и её цвет
-		ax.add_patch(rect) #рисуем квадраты
+		ax.add_patch(rect) #рисование квадратов
 	#вывод полученного графика
 	pyplot.show()
  
-image_path = 'img.jpg' #изначальное изображние
-image = pyplot.imread(image_path) #загружаем изображение в график
-#создаём детектор, используя веса
+image_path = 'img2.jpg' #изначальное изображние
+image = pyplot.imread(image_path) #загруженное изображение в график
+#создание детектора с использованием весов
 detector = MTCNN()
 #распознование лиц
 faces = detector.detect_faces(image) 
